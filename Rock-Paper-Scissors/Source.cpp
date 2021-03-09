6549c54ed60e693c717df150a55d34153c4e4668
kingm-cyber <mking811@g.rwu.edu>
2021-03-09 15:15:08 -0500

    Second commit. Finished rock paper scissors

--- Rock-Paper-Scissors/Source.cpp
+++ Rock-Paper-Scissors/Source.cpp
@@ -8,66 +8,154 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
+#include <time.h>
 
 using namespace std;
+
+//initialize global variables
+const int rock = 1;
+const int paper = 2;
+const int scissors = 3;
+int userWins = 0, computerWins = 0;
+
+//function that returns a random number between 1 and 3
+int getComputerChoice(void)
+{
+	srand(time(NULL));
+	int randomNumber;
+
+	randomNumber = rand() % 3 + 1;
+	
+	return randomNumber;
+}
+
+//function that deteremines the winner of the game
+void getWinner(int userChoice, int computerChoice)
+{
+	//implement a system using if/else statements to compare who won and print the winner here, there is no returning a value
+	if (userChoice == computerChoice)
+	{
+		cout << "DRAW! Nobody wins this round.\n" << endl;
+	}
+	else if (userChoice == rock && computerChoice == paper)
+	{
+		cout << "The computer won this round! Paper beats rock.\n" << endl;
+		computerWins += 1;
+	}
+	else if (userChoice == rock && computerChoice == scissors)
+	{
+		cout << "You won this round! Rock beats scissors.\n" << endl;
+		userWins += 1;
+	}
+	else if (userChoice == paper && computerChoice == scissors)
+	{
+		cout << "The computer won this round! Scissors beats paper.\n" << endl;
+		computerChoice += 1;
+	}
+	else if (userChoice == paper && computerChoice == rock)
+	{
+		cout << "You won this round! Paper beats rock.\n" << endl;
+		userWins += 1;
+	}
+	else if (userChoice == scissors && computerChoice == rock)
+	{
+		cout << "The computer won this round! Rock beats scissors.\n" << endl;
+		computerWins += 1;
+	}
+	else if (userChoice == scissors && computerChoice == paper)
+	{
+		cout << "You won this round! Scissors beats paper.\n" << endl;
+		userWins += 1;
+	}
+	else
+	{
+		cout << "There was an error when trying to calculate a winner..." << endl;
+	}
+}
 
 int main() 
 {
 	//initialize variables
-	string userCheck;
-
-	const int rock = 1;
-	const int paper = 2;
-	const int scissors = 3;
-
-	int userChoice;
-	int computerChoice;
+	char userCheck;
+	int userChoice, computerChoice;
 
 	//user receives a response to begin playing
 	cout << "Hello Player! Are you ready to play Rock-Paper-Scissors? (y/n)\n" << endl;
-	getline(cin, userCheck);
+	cin >> userCheck;
 
-	if (userCheck == "y" || userCheck == "Y") 
+	//if user types 'y', the game will begin running
+	if (userCheck == 'y' || userCheck == 'Y') 
 	{
-		
-		do 
+		do
 		{
-			//take in the value of the user's choice
-			cout << "Enter the value of your choice: (1-3)\n";
-			cout << "1. Rock";
-			cout << "2. Paper";
-			cout << "3. Scissors" << endl;
-			cin >> userChoice;
+			cout << "\n------------------------------------------\n";
+			cout << "\t\tNEW GAME \n";
+			cout << "------------------------------------------" << endl;
+			do 
+			{
+				//take in the value of the user's choice
+				cout << "Enter the value of your choice: (1-3)\n";
+				cout << "1. Rock\n";
+				cout << "2. Paper\n";
+				cout << "3. Scissors\n" << endl;
+				cin >> userChoice;
 
-			//do a function call to generate the computer's choice
+				//check to make sure the user's choice is within the boundaries
+				if (userChoice < 1 || userChoice > 3)
+				{
+					cout << "Error: Illegal choice.\n" << "Exiting program...\n" << endl;
+					exit(1);
+				}
 
-			//do a function call to compare which player 1 based on the user's choice
+				//function call that generates a random choice for the computer
+				computerChoice = getComputerChoice();
 
-			//ask the user if they want to play again, allowing them to edit the userCheck string and play again or exit the program
+				//function call that deteremines the winner of the game
+				getWinner(userChoice, computerChoice);
+			} while (userWins != 4 && computerWins != 4);
 
-		} while (userCheck == "y" || userCheck == "Y");
+			//print out the results of the game
+			cout << "------------------------------------------\n";
+			cout << "\t\tGAME OVER\n";
+			cout << "------------------------------------------\n" << endl;
+
+			if (userWins > 3) 
+			{
+				cout << "You won the best of 7 match!\n";
+				cout << "User wins:       " << userWins << " \n";
+				cout << "Computer wins:   " << computerWins << " \n" << endl;
+			}
+			else if (computerWins > 3) 
+			{
+				cout << "The computer won the best of 7 match!\n";
+				cout << "User wins:       " << userWins << " \n";
+				cout << "Computer wins:   " << computerWins << " \n" << endl;
+			}
+
+			//reset the wins back to 0
+			userWins = 0;
+			computerWins = 0;
+
+			//prompts user to continue playing
+			cout << "Would you like to play again? (y/n)\n" << endl;
+			cin >> userCheck;
+		} while (userCheck == 'y' || userCheck == 'Y');
+
 	}
-	else if (userCheck == "n" || userCheck == "N") 
+	
+	//this if/else statement is run after the first if statement completes
+	if (userCheck == 'n' || userCheck == 'N') 
 	{
-		cout << "Exiting program...\n" << endl;
+		cout << "Thank you for playing!\n" << "Exiting program...\n" << endl;
 		exit(1);
 	}
 	else 
 	{
-		cout << "ERROR! you didn't enter in one of the two options!" << endl;
+		cout << "ERROR! That is not one of the options!\n" << "Exiting program...\n" << endl;
 		exit(1);
 	}
 
 	return 0;
-
 }
 
-int getComputerChoice (void) 
-{
-	//implement random selection between numbers 1-3
-}
 
-void getWinner (int userChoice, int computerChoice) 
-{
-	//implement a system using if/else statements to compare who won and print the winner here, there is no returning a value
-}
