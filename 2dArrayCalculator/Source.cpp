87510d8bf8d65c5110423ad1902201073416611a
kingm-cyber <mking811@g.rwu.edu>
2021-03-15 18:22:42 -0400

    starting 2dArrayCalculator project

---
+++ 2dArrayCalculator/Source.cpp
@@ -0,0 +1,45 @@
+/*
+	Project 1.2: 2dArrayCalculator
+	Created by: Matthew King
+	SEC.205 C++
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <time.h>
+
+
+using namespace std;
+
+int main () 
+{
+	
+	//initialize variables
+	int numberOfValues1;
+	int numberOfValues2;
+	
+	float input1;
+	float input2;
+
+
+
+	//take in input from user
+	cout << "How many values would you like to add to the first array?" << endl;
+	cin >> numberOfValues1;
+
+	for(int i = 0; i < numberOfValues1; i++) 
+	{
+		
+	}
+
+
+	//user should be able to enter values for two different two-dimensional arrays
+
+	//user should be able to add, subtract, divide, and multiply arrays
+
+	//results should be printed to the screen
+
+}
+
