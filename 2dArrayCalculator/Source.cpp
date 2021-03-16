6c62c5e12ba40b9d17e8f89236559c488aa9bcb6
kingm-cyber <mking811@g.rwu.edu>
2021-03-16 13:04:16 -0400

    Progress made on 2dArrayCalculator

--- 2dArrayCalculator/Source.cpp
+++ 2dArrayCalculator/Source.cpp
@@ -13,33 +13,136 @@
 
 using namespace std;
 
-int main () 
+//functions
+//prints the array in a grid view
+void printArray (float (*array)[100], int row, int col) 
+{
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			cout << "  " << array[i][j];
+		}
+		cout << "\n" << endl;
+	}
+}
+
+//adds the values in the arrays together
+/*
+float addArrays () 
 {
 	
+}
+*/
+
+//subtracts the values in the arrays
+
+
+//multiplies the values in the arrays together
+
+
+//divides the values in the arrays
+
+
+int main () 
+{
 	//initialize variables
-	int numberOfValues1;
-	int numberOfValues2;
-	
-	float input1;
-	float input2;
+	int row;
+	int col;
+	int userOperation;
+	float inputA;
+	float inputB;
+	float arrayA[100][100];
+	float arrayB[100][100];
+	float arrayFinal[100][100];
+
+	/*
+		INITIALIZE ARRAY OF POINTERS, SO THAT 2D ARRAYS CAN BE RETURNED FROM FUNCTIONS
+		CREATE 4 OPERATION FUNCTIONS
+		FIX DO WHILE LOOP TO RUN UNTIL USER EXITS
+	*/
+
+	//ask user for number of rows and columns
+	cout << "How many rows do you want in your arrays? (1-100): " << endl;
+	cin >> row;
+	cout << "How many columns do you want in your arrays? (1-100): " << endl;
+	cin >> col;
 
 
-
-	//take in input from user
-	cout << "How many values would you like to add to the first array?" << endl;
-	cin >> numberOfValues1;
-
-	for(int i = 0; i < numberOfValues1; i++) 
+	//verify user input
+	if (row <= 0 || col <= 0 ) 
 	{
-		
+		cout << "Eror: You cannot have an array with negative borders!" << endl;
+		exit(1);
+	}
+	else if (row > 100 || col > 100) 
+	{
+		cout << "Error: The size of your array is too large!" << endl;
+		exit(1);
+	}
+	
+
+	//prompts and takes user input for arrayA
+	cout << "Please enter floating point values for the Array 1: \n" << endl;
+
+	for(int i = 0; i < row; i++) 
+	{
+		for (int j = 0; j < col; j++) 
+		{
+			cout << "Enter a number for array[" << i << "][" << j << "]: \t";
+			cin >> inputA;
+			arrayA[i][j] = inputA;
+		}
+	}
+	
+	//prompts and takes user input for arrayB
+	cout << "Please enter 9 floating point values for the first 2d array: \n" << endl;
+
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			cout << "Enter a number for array[" << i << "][" << j << "]: \t";
+			cin >> inputB;
+			arrayB[i][j] = inputB;
+		}
 	}
 
 
-	//user should be able to enter values for two different two-dimensional arrays
+	//this can be used for printing the values of the array in a grid view
+	printArray(arrayA, row, col);
+	printArray(arrayB, row, col);
 
-	//user should be able to add, subtract, divide, and multiply arrays
 
-	//results should be printed to the screen
+	do 
+	{
+		//ask user to select an operation
+		cout << "Select an operation that you would like to complete on your calculator (1-4): ";
+		cout << "-----------------------------------------------------------------------------";
+		cout << "1. Addition\n";
+		cout << "2. Subtraction\n";
+		cout << "3. Multiplication\n";
+		cout << "4. Division\n";
+		cout << "5. Exit" << endl;
+		cin >> userOperation;
 
+	/*
+		switch (userOperation)
+		{
+		case 1: addArrays();
+			break;
+		case 2: subtractArrays();
+			break;
+		case 3: multiplyArrays();
+			break;
+		case 4: cout << "Dividing arrays...\n" << divideArrays() << endl;
+			printArray(//takes in the array final);
+			break;
+		case 5: cout << "Exiting program..." << endl; 
+			exit(1);
+		}
+		*/
+	} while (userOperation > 0 && userOperation < 6);
+		
 }
 
